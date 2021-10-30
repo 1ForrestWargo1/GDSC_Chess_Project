fd8178a52f338e0129cab661a33a4979d68831f9
Todo25 <87049757+toraOzawa@users.noreply.github.com>
2021-10-30 15:38:20 -0400

    man init

--- Man.java
+++ Man.java
@@ -1,10 +1,18 @@
 public class Man {
-    private final boolean isBlack; // black or red
-    private boolean isPromoted;
-    private int xDirection; // should be based on color either 1 or -1
-    private int yDirection; // should be based on color either 1 or -1
+    public final boolean isBlack; // black or red
+    public int[][] directions;
     
-    public Man(color) {
+    public Man(boolean isBlack) {
+        this.isBlack = isBlack;
+        directions = new int[4][2];
+        directions[0] = new int[]{1, 1};
+        directions[1] = new int[]{-1, 1};
+        directions[2] = null;
+        directions[3] = null;
+    }
 
+    public promote() {
+        directions[2] = null;
+        directions[3] = null;
     }
 }
