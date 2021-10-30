454a6e664abdeb82ea2acdec720c8ec24b099299
Todo25 <87049757+toraOzawa@users.noreply.github.com>
2021-10-29 20:13:22 -0400

    basic structure

---
+++ Board.java
@@ -0,0 +1,5 @@
+public interface Board {
+
+    public Board[] getAllBoards(); 
+    
+}
---
+++ Men.java
@@ -0,0 +1,6 @@
+public class Men {
+    private String color; // black or red
+    private int xDirection; // should be based on color either 1 or -1
+    private int yDirection; // should be based on color either 1 or -1
+        
+}
