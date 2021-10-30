15caf7dd7be5a9e466354c283da4b17826f19bba
RevanMinnam <91348686+RevanMinnam@users.noreply.github.com>
2021-10-30 16:05:14 -0400

    Add files via upload

--- TesterClass.java
+++ TesterClass.java
@@ -1,18 +1,24 @@
+
 public class TesterClass implements chessBoard{
     public int val;
+    
     public TesterClass(int val){
         this.val = val;
 
     }
+    
     public chessBoard[] getBoards(){
-        chessBoard[] boards = new chessBoard[2];
-        if(this.val <= 0){
+        chessBoard[] boards = new chessBoard[2]; 
+        if(this.val <= 0){ //returns an empty list
             return null;
+         
         }
+        //x amount of board values
         boards[0] = new TesterClass(this.val -1);
         boards[1] = new TesterClass(this.val -2);
         return boards;
     }
+     
 	public int evaluateScore(){
         return this.val;
     }
