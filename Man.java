c29c17faea87e1d841f4cdda2b6225379f713a01
jwu114 <91348682+jwu114@users.noreply.github.com>
2021-11-04 23:11:26 -0400

    Update Man.java

--- Man.java
+++ Man.java
@@ -15,4 +15,8 @@ public class Man {
         directions[2] = new int[]{1, -1};
         directions[3] = new int[]{-1, -1};
     }
+    
+    public boolean isBlack() {
+    	return isBlack;
+    }
 }
