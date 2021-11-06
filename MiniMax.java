435e618e39686808f215a70718cce0bfe6b5bcf5
victorgu18 <93011254+victorgu18@users.noreply.github.com>
2021-11-06 15:55:19 -0400

    Update MiniMax.java

--- MiniMax.java
+++ MiniMax.java
@@ -4,18 +4,19 @@ public class MiniMax {
 	chessBoard initial; //intial board
 	int depth; //depth of the tree
 	int turns; //number of turns
+	int depthLimit = 6; //limit of the depth
 	
 	
 	
 
-	public int evaluateBoardMax(chessBoard initial) {
+	public int evaluateBoardMax(chessBoard initial, int depth) {
 		chessBoard[] nextb = initial.getBoards();
 		int max = -999;
-		if (nextb == null) {
+		if (nextb == null || depth == depthLimit) {
 			return initial.evaluateScore();
 		}
 		for(chessBoard b: nextb) {
-			int holder = evaluateBoardMin(b);
+			int holder = evaluateBoardMin(b, depth + 1);
 			if (max <= holder) {
 				max = holder;
 			}
@@ -24,14 +25,14 @@ public class MiniMax {
 		
 	}
 	
-	public int evaluateBoardMin(chessBoard initial) {
+	public int evaluateBoardMin(chessBoard initial, int depth) {
 		chessBoard[] nextb = initial.getBoards();
 		int min = 999;
-		if (nextb == null) {
+		if (nextb == null || depth == depthLimit) {
 			return initial.evaluateScore();
 		}
 		for(chessBoard b: nextb) {
-			int holder = evaluateBoardMax(b);
+			int holder = evaluateBoardMax(b, depth + 1);
 			if (min >= holder) {
 				min = holder;
 			}
