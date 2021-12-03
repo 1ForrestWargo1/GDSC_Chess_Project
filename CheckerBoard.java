0b0dfd177e7ab2bb22e80b2bfc7461e752587c26
Todo25 <87049757+toraOzawa@users.noreply.github.com>
2021-12-03 16:42:35 -0500

    interface refactor

--- CheckerBoard.java
+++ CheckerBoard.java
@@ -3,48 +3,50 @@ import java.util.Scanner;
 
 public class CheckerBoard implements Board {
     private Man[][] board; // 2D array of board
-    private boolean blackTurn; 
+    private boolean blackTurn;
     private int moveCount;
 
-    public CheckerBoard() {  // default constructor 
+    public CheckerBoard() { // default constructor
         board = new Man[8][8];
         blackTurn = true;
         moveCount = 0;
         setMen();
     }
 
-    public CheckerBoard(Man[][] board, Boolean turn, int moveCount) { // constructor which makes a new board from a pre-existing board
+    public CheckerBoard(Man[][] board, Boolean turn, int moveCount) { // constructor which makes a new board from a
+                                                                      // pre-existing board
         this.board = new Man[8][8];
         // if (board == null) System.out.println("board is orignally null");
         blackTurn = turn;
         this.moveCount = moveCount;
-        
+
         setMen(board);
         // if (board == null) System.out.println("board is orignally null");
     }
 
     public void setMen() { // sets pieces in default positions
-       for (int i = 0; i < 3; i++) {
-           for(int j = 0; j < board[i].length; j++) {
-               if ((i + j + 1) % 2 == 0) {
-                   board[i][j] = new Man(false);
-               }
-           }
-       }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if ((i + j + 1) % 2 == 0) {
+                    board[i][j] = new Man(false);
+                }
+            }
+        }
 
-       for (int i = 5; i < 8; i++) {
-           for(int j = 0; j < board[i].length; j++) {
+        for (int i = 5; i < 8; i++) {
+            for (int j = 0; j < board[i].length; j++) {
                 if ((i + j + 1) % 2 == 0) {
                     board[i][j] = new Man(true); // bottom side pieces are black and go first
                 }
-           }
-       }
+            }
+        }
     }
 
     public void setMen(Man[][] board) { // sets pieces to the same positions the board passed in
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] != null) this.board[i][j] = new Man(board[i][j].isBlack);
+                if (board[i][j] != null)
+                    this.board[i][j] = new Man(board[i][j].isBlack);
                 // else this.board[i][j] = null;
             }
         }
@@ -56,11 +58,13 @@ public class CheckerBoard implements Board {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 Man currentMan = board[j][i]; // pointer to coordinate
-                if (currentMan != null && currentMan.isBlack == blackTurn) { // if piece exists at coordinate and the piece is for the correct turn
-                    for (int z = 0; z < currentMan.directions.length; z++) { 
-                        if (currentMan.directions[z] != null) {  // for every element in the direction array 
-                            Board result = (Board) this.move(currentMan.directions[z], j, i); // simulate the move 
-                            if (result != null) boards.add(result); // add it to the array 
+                if (currentMan != null && currentMan.isBlack == blackTurn) { // if piece exists at coordinate and the
+                                                                             // piece is for the correct turn
+                    for (int z = 0; z < currentMan.directions.length; z++) {
+                        if (currentMan.directions[z] != null) { // for every element in the direction array
+                            Board result = (Board) this.move(currentMan.directions[z], j, i); // simulate the move
+                            if (result != null)
+                                boards.add(result); // add it to the array
                         }
                     }
                 }
@@ -75,57 +79,60 @@ public class CheckerBoard implements Board {
 
     // }
 
-    public CheckerBoard move(int[] directions, int i, int j) { // used in getAllBoards and returns the board result from the "move" inputted; returns null if not valid
-        CheckerBoard result = new CheckerBoard(this.board, !this.blackTurn, moveCount+1);
+    public CheckerBoard move(int[] directions, int i, int j) { // used in getAllBoards and returns the board result from
+                                                               // the "move" inputted; returns null if not valid
+        CheckerBoard result = new CheckerBoard(this.board, !this.blackTurn, moveCount + 1);
         Man[][] currentBoard = result.board; // create a copy of current chess board
-		// if (result.board == null) System.out.println("constructor failed");
-		Man man = currentBoard[i][j];
-		
-		int y;
-		int x;
-		
-		if(man.isBlack()) {
-			 y = directions[0];
-			 x = directions[1];
-		} else {
-			 y = -directions[0];
-			 x = -directions[1];
-		}
+        // if (result.board == null) System.out.println("constructor failed");
+        Man man = currentBoard[i][j];
 
-	    if (!(i + y >= 0 && i + y <= 7 && j + x >= 0 && j + x <= 7)) {
-			return null;
-		}
-	    
-		if (!man.isBlack()) { //white checker (at top)
-			if (currentBoard[i + y][j + x] == null) {
-					currentBoard[i + y][j + x] = currentBoard[i][j]; // move white checker
+        int y;
+        int x;
+
+        if (man.isBlack()) {
+            y = directions[0];
+            x = directions[1];
+        } else {
+            y = -directions[0];
+            x = -directions[1];
+        }
+
+        if (!(i + y >= 0 && i + y <= 7 && j + x >= 0 && j + x <= 7)) {
+            return null;
+        }
+
+        if (!man.isBlack()) { // white checker (at top)
+            if (currentBoard[i + y][j + x] == null) {
+                currentBoard[i + y][j + x] = currentBoard[i][j]; // move white checker
+                currentBoard[i][j] = null;
+            } else if (currentBoard[i + y][j + x].isBlack()) {
+                if (i + y + y >= 0 && i + y + y <= 7 && j + x + x >= 0 && j + x + x <= 7
+                        && currentBoard[i + y + y][j + x + x] == null) {
+                    currentBoard[i + y][j + x] = null; // kill the black checker
+                    currentBoard[i + y + y][j + x + x] = currentBoard[i][j]; // move white checker
                     currentBoard[i][j] = null;
-			} else if (currentBoard[i + y][j + x].isBlack()) {
-				if (i + y + y >= 0 && i + y + y <= 7 && j + x + x >= 0 && j + x + x <= 7 && currentBoard[i + y + y][j + x + x] == null) {
-					currentBoard[i + y][j + x] = null; // kill the black checker
-					currentBoard[i + y + y][j + x + x] = currentBoard[i][j]; // move white checker
-                    currentBoard[i][j] = null;
-				}
-			} else {
+                }
+            } else {
                 // System.out.println("move is inavlid");
-				return null;
-			}
-		} else { //black checker (at bottom)
-			if (currentBoard[i + y][j + x] == null) {
-					currentBoard[i + y][j + x] = currentBoard[i][j]; // move black checker
-                    currentBoard[i][j] = null;
-			} else if (!currentBoard[i + y][j + x].isBlack()) {
-				if (i + y + y >= 0 && i + y + y <= 7 && j + x + x >= 0 && j + x + x <= 7 && currentBoard[i + y + y][j + x + x] == null) {
-					currentBoard[i + y][j + x] = null; // kill the white checker
-					currentBoard[i + y + y][j + x + x] = currentBoard[i][j];
+                return null;
+            }
+        } else { // black checker (at bottom)
+            if (currentBoard[i + y][j + x] == null) {
+                currentBoard[i + y][j + x] = currentBoard[i][j]; // move black checker
+                currentBoard[i][j] = null;
+            } else if (!currentBoard[i + y][j + x].isBlack()) {
+                if (i + y + y >= 0 && i + y + y <= 7 && j + x + x >= 0 && j + x + x <= 7
+                        && currentBoard[i + y + y][j + x + x] == null) {
+                    currentBoard[i + y][j + x] = null; // kill the white checker
+                    currentBoard[i + y + y][j + x + x] = currentBoard[i][j];
                     currentBoard[i][j] = null; // move black checker
-				}
-			} else {
+                }
+            } else {
                 // System.out.println("move is inavlid");
-				return null;
-			}
-		}
-		return result;
+                return null;
+            }
+        }
+        return result;
     }
 
     public void makeMove(CheckerBoard nextBoard) { // actually makes the move on this. board
@@ -133,7 +140,6 @@ public class CheckerBoard implements Board {
         blackTurn = !blackTurn;
         moveCount++;
     }
-
 
     public boolean isOver() { // checks if game is over
         int blackCount = 0;
@@ -146,15 +152,14 @@ public class CheckerBoard implements Board {
                     } else {
                         whiteCount++;
                     }
-                } 
+                }
             }
         }
         return blackCount == 0 || whiteCount == 0;
     }
 
-
     // getters and setters
-    public Man[][] getBoard() {  // returns current board state
+    public Man[][] getBoard() { // returns current board state
         return this.board;
     }
 
@@ -206,11 +211,13 @@ public class CheckerBoard implements Board {
                     } else {
                         whiteCount++;
                     }
-                } 
+                }
             }
         }
-        if (blackCount == 0) return -13; // if the game is won return 13 or -13
-        else if (whiteCount == 0) return 13;
+        if (blackCount == 0)
+            return -13; // if the game is won return 13 or -13
+        else if (whiteCount == 0)
+            return 13;
         return blackCount - whiteCount; // otherwise return the difference in the number of pieces
     }
 
@@ -220,32 +227,41 @@ public class CheckerBoard implements Board {
 
         int choiceIndex = 0;
 
-        while(answer.equals("yes")) {
+        while (answer.equals("yes")) {
             printBoard();
             System.out.println();
 
             ArrayList<Board> allPossibleNext = getAllBoards();
             System.out.println("All possible boards: ");
             for (int i = 0; i < allPossibleNext.size(); i++) {
-                CheckerBoard current = (CheckerBoard)(allPossibleNext.get(i));
-                System.out.println(i+":");
+                CheckerBoard current = (CheckerBoard) (allPossibleNext.get(i));
+                System.out.println(i + ":");
                 current.printBoard();
                 System.out.println();
             }
             System.out.print("Choose next board");
             choiceIndex = scn.nextInt();
             System.out.println();
-            makeMove((CheckerBoard)(allPossibleNext.get(choiceIndex)));
+            makeMove((CheckerBoard) (allPossibleNext.get(choiceIndex)));
             System.out.println("New board: ");
             printBoard();
             System.out.println();
 
-            // System.out.print("Would you like to make another move? (enter yes to continue, no to terminate program) ");
+            if (this.isOver()) {
+                if (evaluateBoard() > 0) {
+                    System.out.println("Red won");
+                } else {
+                    System.out.println("White won");
+                }
+            }
+
+            // System.out.print("Would you like to make another move? (enter yes to
+            // continue, no to terminate program) ");
             // answer = scn.nextLine();
-            
+
         }
         System.out.println("program ended");
-        scn.close();   
+        scn.close();
     }
 
     public static void main(String args[]) {
--- Chess/Rook.java
+++ Chess/Rook.java
@@ -31,45 +31,37 @@ public class Rook implements Piece {
     }
 
     private void updateScope(Square currentSquare, int x, int y) {
-        if(currentSquare == null) {
+        if (currentSquare == null) {
             return;
-        }
-        else if(x > 4) {
+        } else if (x > 4) {
             updateScope(currentSquare.right, x + 1, y);
-        }
-        else if(x < 4) {
+        } else if (x < 4) {
             updateScope(currentSquare.left, x - 1, y);
-        }
-        else if(y > 4) {
+        } else if (y > 4) {
             updateScope(currentSquare.right, x, y + 1);
-        }
-        else if(y < 4) {
+        } else if (y < 4) {
             updateScope(currentSquare.down, x, y - 1);
         }
         scope[y][x] = currentSquare;
     }
 
     public ArrayList<Square> getMoves(int x, int y, ArrayList<Square> squares) {
-        if(scope[y][x] == null) {
+        if (scope[y][x] == null) {
             return squares;
-        }
-        else if(x > 4) {
-            if(scope[y][x].isEmpty()) {
+        } else if (x > 4) {
+            if (scope[y][x].isEmpty()) {
                 getMoves(x + 1, y, squares);
             }
-        }
-        else if(x < 4) {
-            if(scope[y][x].isEmpty()) {
+        } else if (x < 4) {
+            if (scope[y][x].isEmpty()) {
                 getMoves(x - 1, y, squares);
             }
-        }
-        else if(y > 4) {
-            if(scope[y][x].isEmpty()) {
+        } else if (y > 4) {
+            if (scope[y][x].isEmpty()) {
                 getMoves(x, y + 1, squares);
             }
-        }
-        else if(y < 4) {
-            if(scope[y][x].isEmpty()) {
+        } else if (y < 4) {
+            if (scope[y][x].isEmpty()) {
                 getMoves(x, y - 1, squares);
             }
         } else {
@@ -79,15 +71,15 @@ public class Rook implements Piece {
             getMoves(x, y - 1, squares);
         }
 
-        if(scope[y][x].isEmpty() | scope[y][x].piece().isWhite() != this.isWhite) {
+        if (scope[y][x].isEmpty() | scope[y][x].piece().isWhite() != this.isWhite) {
             squares.add(scope[y][x]);
         }
         return squares;
     }
 
-    public void makeMove(Square currentSquare, Square endSquare) {
-        endSquare.setPiece(currentSquare.piece());
-        currentSquare.setPiece(null);
+    public void makeMove(Square endSquare) {
+        endSquare.setPiece(scope[4][4].piece());
+        scope[4][4].setPiece(null);
         updateScope(endSquare, 4, 4);
     }
 }
